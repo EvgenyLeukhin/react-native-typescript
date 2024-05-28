69d279d5836fa5077110a38a044414ed368e617e
Evgeny Leukhin <EvgenyLeukhin@yandex.ru>
2024-05-28 22:18:50 +0600

    Update

--- App.tsx
+++ App.tsx
@@ -1,15 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+const App = () => (
+  <View style={styles.container}>
+    <Text>Open up App.tsx to start working on your app</Text>
+    <StatusBar style="auto" />
+  </View>
+);
 
+// styles
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -18,3 +17,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
